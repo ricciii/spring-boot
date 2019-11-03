64c71c49b01b539f965e32772bb5ba08fb831c9d
Rich Fosgate <reichneyii@gmail.com>
2019-11-03 21:02:38 +0800

    add more view

--- controller/src/main/java/com/ecc/exercises/springexercise/RoleController.java
+++ controller/src/main/java/com/ecc/exercises/springexercise/RoleController.java
@@ -1,25 +1,37 @@
 package com.ecc.exercises.springexercise.controller;
 
+import com.ecc.exercises.springexercise.model.Role;
 import com.ecc.exercises.springexercise.service.RoleService;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@RestController
+@Controller
 @RequestMapping(value = "/roles")
 public class RoleController {
 
 	@Autowired
 	RoleService roleService;
-   
-	@RequestMapping(value = "")
-	public String role() {
-    	return "roles";
+   	List<Role> roles;
+	@GetMapping(value = "")
+	public String roles() {
+    	return "roles/list";
 	}
 
-	@RequestMapping(value = "/new")
-	public String createRole() {
-    	return "roles";
+	@GetMapping(value = "/list")
+	public String listRoles() {
+    	return "roles/list";
+	}
+
+	@PostMapping(value = "/create-role")
+	public String createRole(Role role) {
+    	return "roles/create-role";
 	}
 }
