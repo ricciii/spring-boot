1a5275ffd14032db07d845329932a428c49d936e
Reichney Fosgate II <reichneyfosgateii@gmail.com>
2019-11-08 16:05:17 +0800

    clear

--- util/src/main/java/com/ecc/exercises/springexercise/HibernateUtil.java
+++
@@ -1,9 +0,0 @@
-package com.ecc.exercises.springexercise.util;
-
-import org.hibernate.Session; 
-import org.hibernate.Transaction;
-import org.hibernate.SessionFactory;
-
-public interface HibernateUtil {
-	public SessionFactory getSessionFactory();
-}
